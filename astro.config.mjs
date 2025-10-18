1812f830347906383be050c9b42d475ac41e3435
Ric Dodong <ricalgen@gmail.com>
2025-10-18 13:15:07 +0800

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
